893d6d74fb9d5f49c6fa004668c1ef03cf089e0d
AX1001 <01antoniocastellano@gmail.com>
2024-03-16 20:55:49 +0100

    fix and format for spanish translation

--- js/languages/lang-es.js
+++ js/languages/lang-es.js
@@ -1,3 +1,5 @@
+// This is main language file! It's Spanish!
+
 LANGUAGES.ES = {
     name: "Spanish",
     inter_name: "Español",
@@ -9,8 +11,8 @@ LANGUAGES.ES = {
         'fish-name': "Peces",
         'fish-costName': toTextStyle('Peces','fish'),
 
-        'prestige-name': "Fragmentos de prestigio",
-        'prestige-costName': toTextStyle('Prestigio','prestige') + " Fragmentos",
+        'prestige-name': "Cristales de prestigio",
+        'prestige-costName': "Cristales de " + toTextStyle('Prestigio','prestige'),
 
         'coral-name': "Corales",
         'coral-costName': toTextStyle('Corales','coral'),
@@ -28,7 +30,7 @@ LANGUAGES.ES = {
         'core-costName': 'Fragmentos ' + toTextStyle('Magmáticos','core'),
 
         'curr-top-0-req': x => `Alcanza <b>${format(x)}</b> ${toTextStyle('Peces','fish')} en total`, 
-        'curr-top-0-reset': x => `Prestigia para conseguir <b>${x.format(0)}</b> cristales de ${toTextStyle('Prestigio','prestige')}`,
+        'curr-top-0-reset': x => `Haz prestigio para conseguir <b>${x.format(0)}</b> cristales de ${toTextStyle('Prestigio','prestige')}`,
 
         'curr-top-1-req': x => `Alcanza un total de <b>${format(x)}</b> cristales de ${toTextStyle('Prestigio','prestige')}`, 
         'curr-top-1-reset': x => `Entra al núcleo para conseguir <b>${x.format(0)}</b> fragmentos ${toTextStyle('Magmáticos','core')}`,
@@ -41,14 +43,14 @@ LANGUAGES.ES = {
         'tab-research': toTextStyle('Investigaciones','prestige'),
         'tab-explore': "Exploración",
         'tab-core': "El "+toTextStyle('Núcleo','core'),
-        'tab-core-reactor': "Reactor" +toTextStyle('Núclear','core'),
+        'tab-core-reactor': "Reactor " + toTextStyle('Núclear','core'),
 
         // Elements
 
         'fish-div': `Tu ${toTextStyle('Tiburón','shark')} ha comido <h2>${toTextStyle('0','fish','fish-amount')}</h2> <span id="fish-gain"></span> peces.`,
         'shark-stats': `Estadísticas del ${toTextStyle('Tiburón','shark')}<br>Nivel: <h4 id="shark-level">???</h4><br>Rango: <h4 id="shark-tier">???</h4>`,
 
-        'option-title-1': "Opciones principales",
+        'option-title-1': "Opciones Principales",
         'option-title-2': "Notaciones",
         'option-title-3': "Confirmaciones",
         'option-title-4': "Idiomas",
@@ -57,34 +59,34 @@ LANGUAGES.ES = {
 
         'su-s1-req': "Nivel 3",
         'su-s1-name': 'Fuerza del Tiburón',
-        'su-s1-desc': `Aumenta los ${toTextStyle('Peces','fish')} comidos por <b>+1</b> por nivel.`,
+        'su-s1-desc': `Aumenta los ${toTextStyle('Peces','fish')} comidos en <b>+1</b> por nivel.`,
 
         'su-s2-req': "Nivel 7",
-        'su-s2-name': 'Agilidad del Tiburón',
-        'su-s2-desc': `Aumenta la base de los niveles del ${toTextStyle('Tiburón','shark')} por un bonus de ${toTextStyle('Peces','fish')} por <b>+1</b> por nivel.`,
+        'su-s2-name': 'Agilidad de Tiburón',
+        'su-s2-desc': `Aumenta la base de los niveles del ${toTextStyle('Tiburón','shark')} en un bonus de ${toTextStyle('Peces','fish')} de <b>+1</b> por nivel.`,
 
         'su-s3-req': "Nivel 15",
-        'su-s3-name': 'Dientes del Tiburón',
-        'su-s3-desc': `Aumenta el exponente de la primera mejora del ${toTextStyle('Tiburón','shark')} por un <b>+50%</b> por nivel.`,
+        'su-s3-name': 'Dientes de Tiburón',
+        'su-s3-desc': `Aumenta el exponente de la primera mejora del ${toTextStyle('Tiburón','shark')} en un <b>+50%</b> por nivel.`,
 
         'su-s4-req': "Nivel 38",
         'su-s4-name': 'Exponente del Tiburón',
-        'su-s4-desc': `Aumenta el exponente de los ${toTextStyle('Peces','fish')} por <b>+1%</b> por nivel.`,
+        'su-s4-desc': `Aumenta el exponente de los ${toTextStyle('Peces','fish')} en <b>+1%</b> por nivel.`,
 
         'su-p1-req': "Primer Prestigio",
         'su-p1-name': 'Fuerza de Super Tiburón',
-        'su-p1-desc': `Aumenta los ${toTextStyle('Peces','fish')} comidos por <b>×3</b> por nivel.`,
+        'su-p1-desc': `Aumenta los ${toTextStyle('Peces','fish')} comidos en <b>×3</b> por nivel.`,
 
         'su-p2-req': "Primer Prestigio",
         'su-p2-name': 'Potenciador de prestigio',
-        'su-p2-desc': `Aumenta los ${toTextStyle('Peces','fish')} comidos por <b>×lg(${toTextStyle('Peces','fish')})</b> por nivel.`,
+        'su-p2-desc': `Aumenta los ${toTextStyle('Peces','fish')} comidos en <b>×lg(${toTextStyle('Peces','fish')})</b> por nivel.`,
 
-        'su-p3-name': 'Tiburón Escalado',
-        'su-p3-desc': `Aumenta la primera escala de los niveles del ${toTextStyle('Tiburón','shark')} comenzando por <b>+1</b> por nivel.`,
+        'su-p3-name': 'Escalado de Tiburón',
+        'su-p3-desc': `Aumenta el primer escalado de los niveles del ${toTextStyle('Tiburón','shark')} comenzando en <b>+1</b> por nivel.`,
 
         // Researches
 
-        'research-p1-name': "Agilidad prestigiosa",
+        'research-p1-name': "Agilidad Prestigiosa",
         'research-p1-desc': `'Agilidad del Tiburón' ahora afecta a la base de los niveles del ${toTextStyle("Tiburón",'shark')} bonus de cristales de ${toTextStyle("Prestigio",'prestige')} por una baja cantidad .`,
 
         'research-p2-name': "Mejora de Tiburón EL",
@@ -107,20 +109,20 @@ LANGUAGES.ES = {
         'research-p7-name': "Mejores recursos I",
         'research-p7-desc': `El efecto de los ${toTextStyle("Corales",'coral')} y el ${toTextStyle("Hielo",'ice')} son mejores.`,
 
-        'research-e1-name': "Sinergismo oceánico I",
+        'research-e1-name': "Sinergia oceánica I",
         'research-e1-desc': `La ${toTextStyle("Sal",'salt')} impulsa los ${toTextStyle("Corales",'coral')} generados.`,
 
-        'research-e2-name': "Sinergismo oceánico II",
+        'research-e2-name': "Sinergia oceánica II",
         'research-e2-desc': `La ${toTextStyle("Compressed Nieve",'snow')} impulsa el ${toTextStyle("Hielo",'ice')} generado.`,
 
-        'research-c1-name': "Investigacione C-Keeper",
+        'research-c1-name': "Investigaciones C-Keeper",
         'research-c1-desc': `Manten tus ${toTextStyle("Investigaciones",'prestige')} al entrar al ${toTextStyle('Núcleo','core')}.`,
 
         'research-c2-name': "Exploración C-Keeper",
         'research-c2-desc': `Mantén los primeros <b>X</b> océanos desbloqueados y su profundidad al entrar al núcleo ${toTextStyle('Núcleo','core')}.`,
 
         'research-c3-name': "Afecto de Neón I",
-        'research-c3-desc': `El <b>Neón</b> ahora afecta a los 'Dientes del Tiburón', 'Exponente del Tiburón', y a 'Tiburón Escalado'.`,
+        'research-c3-desc': `El <b>Neón</b> ahora afecta a los 'Dientes de Tiburón', 'Exponente del Tiburón', y a 'Escalado de Tiburón'.`,
 
         'research-c4-name': "Mejor Níquel",
         'research-c4-desc': `El efecto del <b>Níquel</b> es el <b>doble</b> de poderoso.`,
@@ -134,23 +136,23 @@ LANGUAGES.ES = {
         'explore-next': x=>`Nuevo océano al conseguir el nivel <h3>${format(x,0)}</h3> del ${toTextStyle("Tiburón",'shark')}.`,
 
         'explore-doubler-1': x=>`Duplica ${x} ganado.`,
-        'explore-doubler-2': `Duplica la progression de la profundidad ganada.`,
+        'explore-doubler-2': `Duplica la progresión conseguida de profundidad.`,
 
         'explore-0-name': `Océano Pacífico`,
         'explore-0-desc': `Los ${toTextStyle('Peces','fish')} ganados son enraizados por 2.`,
         'explore-0-milestone': [
             `Aumenta los cristales de ${toTextStyle('Prestigio','prestige')} ganados elevados a 1.05.`,
-            `Aumenta los recursos y el progreso de la profundidad por un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
+            `Aumenta los recursos y el progreso de la profundidad en un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
             `Nivelar el ${toTextStyle('Tiburón','shark')} ya no gasta ${toTextStyle('Peces','fish')}.`,
             `Aumenta los ${toTextStyle('Peces','fish')} comidos a la potencia de 1.05.`,
             `Desbloquea el ${toTextStyle('Núcleo','core')}.`,
         ],
 
         'explore-1-name': `Océano Ártico`,
-        'explore-1-desc': `Resetéa tus cristales de ${toTextStyle("Prestigio",'prestige')} y mejoras de ${toTextStyle("Prestigio",'prestige')}.La ganancia de los cristales de ${toTextStyle('Prestigio','prestige')} es enraizada por 2.`,
+        'explore-1-desc': `Reinicia tus cristales de ${toTextStyle("Prestigio",'prestige')} y mejoras de ${toTextStyle("Prestigio",'prestige')}. La ganancia de los cristales de ${toTextStyle('Prestigio','prestige')} es enraizada por 2.`,
         'explore-1-milestone': [
             `Desbloquea Auto-mejoras de ${toTextStyle('Prestigio','prestige')} del ${toTextStyle("Tiburón","shark")}.`,
-            `Aumenta los recursos y el progreso de la profundidad por un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
+            `Aumenta los recursos y el progreso de la profundidad en un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
             `Las mejoras de ${toTextStyle('Prestigio','prestige')} del ${toTextStyle('Tiburón','shark')} ya no gastan cristales de ${toTextStyle('Prestigio','prestige')}.`,
             `Genera pasivamente el 100% de los cristales de ${toTextStyle('Prestigio','prestige')} que ganarías en un ${toTextStyle('Prestigio','prestige')}.`,
         ],
@@ -159,17 +161,17 @@ LANGUAGES.ES = {
         'explore-2-desc': `Resetéa tus cristales de ${toTextStyle("Prestigio",'prestige')} y mejoras de ${toTextStyle("Prestigio",'prestige')}.Nivelar el ${toTextStyle('Tiburón','shark')} escala 10x más rápido.` ,
         'explore-2-milestone': [
             `El escalamiento de los 'Dientes del Tiburón' y 'Exponente del Tiburón' son ligeramente más débiles.`,
-            `Aumenta los recursos y el progreso de la profundidad por un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
+            `Aumenta los recursos y el progreso de la profundidad en un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
             `La escala de los niveles del ${toTextStyle('Tiburón','shark')} empiezan +1 más tarde por cada 500m de profundidad.`,
-            `El efecto del océano es un 50% más poderoso.`,
+            `El efecto del océano es un 50% más debil.`,
         ],
 
-        'explore-3-name': `Oceáno Meridional`,
+        'explore-3-name': `Oceáno Antártico`,
         'explore-3-desc': `Resetéa tus cristales de ${toTextStyle("Prestigio",'prestige')} y mejoras de ${toTextStyle("Prestigio",'prestige')}. No puedes comprar mejoras de ${toTextStyle('Peces','fish')} y ${toTextStyle('Prestigio','prestige')} del ${toTextStyle('Tiburón','shark')}.`,
         'explore-3-milestone': [
-            `Aumenta la base de la exploración por 10x.`,
-            `Aumenta los recursos y el progreso de la profundidad por un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
-            `El efecto del océano es un 50% más poderoso.`,
+            `Aumenta la base de la exploración en 10x.`,
+            `Aumenta los recursos y el progreso de la profundidad en un 25% por cada raíz cuadrada de los niveles del ${toTextStyle('Tiburón','shark')}.`,
+            `El efecto del océano es un 50% más debil.`,
         ],
 
         // Core Reactor
@@ -186,7 +188,7 @@ LANGUAGES.ES = {
         'core-3-name': `Neón`,
         'core-3-desc': `Los niveles del ${toTextStyle("Tiburón",'shark')} bajan sus requisitos basados en los niveles de <b>Neón</b>.`,
 
-        'core-bonus': x => `Los reactores del núcleo impulsarán los ${toTextStyle("Peces",'fish')} por <h4>${formatMult(x)}</h4>.`,
+        'core-bonus': x => `Los reactores del núcleo impulsarán los ${toTextStyle("Peces",'fish')} en <h4>${formatMult(x)}</h4>.`,
 
         // Automation
 
@@ -195,7 +197,7 @@ LANGUAGES.ES = {
         'auto-spu-name': `Auto-mejoras de ${toTextStyle('Prestigio','prestige')} del ${toTextStyle("Tiburón","shark")}`,
         'auto-eu-name': `Auto-mejoras de exploración`,
 
-        'auto-cost': (D,cost,name) => `Disminuye el intérvalo por un ${formatReduction(D,0)}.<br>Precio: ${format(cost,0)} ${name}`,
+        'auto-cost': (D,cost,name) => `Disminuye el intérvalo en ${formatReduction(D,0)}.<br>Precio: ${format(cost,0)} ${name}`,
         'auto-interval': (a,b,maxed) => `Intérvalo: ${format(a,3)}s`+(maxed ? "" :` ➜ ${format(b,3)}s`),
 
         // Progress
@@ -219,7 +221,7 @@ LANGUAGES.ES = {
             let p = toTextStyle('Prestigio','prestige'), s = toTextStyle('Tiburón','shark'), f = toTextStyle('Peces','fish')
             return `
             <h3>${p}</h3><br>
-            El ${p} es la primera capa de reseteo. Prestigiar reseteará las mejoras de ${s}, ${s} , y los ${f} por cristales de ${p} .
+            El ${p} es la primera capa de reinicio. Hacer prestigio reiniciara las mejoras de ${s}, ${s} , y los ${f} por cristales de ${p} .
             El primer ${p} desbloqueará mejoras de ${s}.<br>
             <img src="textures/PrestigeShard.png"><br>
             ¿Estás seguro que quieres hacer un prestigio?
@@ -229,7 +231,7 @@ LANGUAGES.ES = {
             let c = toTextStyle('Núcleo','core'), m = toTextStyle('Magmáticos','core'), p = toTextStyle('Prestigio','prestige')
             return `
             <h3>El ${c}</h3><br>
-            El ${c} es la segunda capa de reseteo. Entrar al núcleo reseteará todo lo que el ${p} hace, también como los cristales de ${p} , mejoras de ${p} , ${toTextStyle('Investigaciones','prestige')} (casi todos), y exploraciones por fragmentos ${m},
+            El ${c} es la segunda capa de reinicio. Entrar al núcleo reiniciara todo lo que el ${p} hace, también como los cristales de ${p} , mejoras de ${p} , ${toTextStyle('Investigaciones','prestige')} (casi todas), y exploraciones por fragmentos ${m},
             Desbloqueando el reactor del ${c} por la primera vez.<br>
             <img src="textures/Magmatic.png"><br>
             ¿Estás seguro que quieres entrar al núcleo?
@@ -247,7 +249,7 @@ LANGUAGES.ES = {
         'effect': "Effecto",
         'cost': "Precio",
         'buyMax': "Comprar máximo",
-        'require': "Require",
+        'require': "Requiere",
         'next-at': "Siguiente en",
         'depth': "Profundidad",
 
@@ -267,11 +269,11 @@ LANGUAGES.ES = {
         },
 
         'notify-desc' : {
-            save: "!Juego Salvado!"
+            save: "!Juego guardado!"
         },
 
         'radio-desc' : {
-            'notation': ['Formato de notación',['Científico','Estándar','Científico Mezclado']],
+            'notation': ['Formato de notación',['Científica','Estándar','Científica Mezclada']],
             'comma-format': ['Máximos OoMs de comas en números',['3','6','9','12','15']],
             'autosave': ['Autoguardado',['Desactivado','Activado']],
             'autosave-time': ['Intervalo de Autoguardado',['15s','30s','60s','120s']],
@@ -282,9 +284,9 @@ LANGUAGES.ES = {
 
         'prompt-placeholder': "Escribe aquí para...",
 
-        'option-buttons-text': ["Guardar","Exportar por portapapeles","Exportar por archivo","Importar por texto","Importar por archivo","¡¡¡BORRAR PROGRESO!!!"],
+        'option-buttons-text': ["Guardar","Exportar al portapapeles","Exportar en un archivo","Importar por texto","Importar de un archivo","¡¡¡BORRAR PROGRESO!!!"],
 
-        'confirm-prestige': "Hazer prestigio",
+        'confirm-prestige': "Hacer prestigio",
         'confirm-core': "Entrar al núcleo",
     },
 }
